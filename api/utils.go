06552b75292c820322a31990235f04d03d6d317b
Brandon To <brandon_to@hotmail.com>
2024-05-01 17:06:08 -0400

    Implemented isPalindrome() function

--- api/utils.go
+++ api/utils.go
@@ -3,6 +3,7 @@ package api
 import (
     "errors"
     "strings"
+    "unicode"
 )
 
 // Evaluates: "1" and "true" to true
@@ -20,7 +21,29 @@ func stringToBool(s string) (bool, error) {
     }
 }
 
-func isPalindrome(s string) bool {
-    // TODO
+func isPalindrome(str string) bool {
+    // Lowercase and remove all non alphanumeric characters from the input
+    // string
+    //
+    strippedStr := strings.Map(func(r rune) rune {
+        if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+            return -1
+        }
+        return unicode.ToLower(r)
+    }, str)
+
+    // Convert string to rune array
+    //
+    runeArr := []rune(strippedStr)
+
+    // Iterate through the string from both sides, comparing the results until
+    // they overlap
+    //
+    for i, j := 0, len(runeArr)-1; i < j; i, j = i+1, j-1 {
+        if runeArr[i] != runeArr[j] {
+            return false
+        }
+    }
+
     return true
 }
