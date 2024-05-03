4b4d60ac9aa140a2d701d16ef6b94cfa35b8f611
Brandon To <brandon_to@hotmail.com>
2024-05-03 01:58:55 -0400

    Added swagger generated documentation in the form of a web page.

--- api/router.go
+++ api/router.go
@@ -1,6 +1,14 @@
 package api
 
 import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/brandonto/rest-api-microservice-demo/db"
 
 	"github.com/go-chi/chi/v5"
@@ -24,6 +32,25 @@ func NewRouter(svcDb *db.Db, enableLogger bool) chi.Router {
 	//
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 
+	// Reads openapi.json
+	//
+	openApiFileBytes, err := ioutil.ReadFile("docs/openapi.json")
+	if err != nil {
+		log.Fatal(errors.New("Unable to open openapi.json file"))
+	}
+
+	// GET /openapi.json
+	//
+	r.Get("/openapi.json", func (w http.ResponseWriter, r *http.Request) {
+		w.Write(openApiFileBytes)
+	})
+
+	// GET /swagger
+	//
+	workDir, _ := os.Getwd()
+	htmlDir := http.Dir(filepath.Join(workDir, "html"))
+	FileServer(r, "/swagger", htmlDir)
+
 	// Configure API routes
 	//
 	r.Route("/messages", func(r chi.Router) {
@@ -39,4 +66,29 @@ func NewRouter(svcDb *db.Db, enableLogger bool) chi.Router {
 	})
 
 	return r
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+//
+// This code snippet is taken from:
+// https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
+//
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
