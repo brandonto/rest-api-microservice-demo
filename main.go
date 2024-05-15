b4939a6fad235bd05cc3d6ee6fb023b444ccc0b4
João Machado <jtomaspm@ua.pt>
2024-05-15 22:47:56 +0100

    added docker files and env variables

--- main.go
+++ main.go
@@ -9,60 +9,40 @@ import (
 	"github.com/brandonto/rest-api-microservice-demo/db"
 )
 
-const defaultDbBucketName = "DetailedMessageBucket"
-const defaultPort = uint64(55555)
+const (
+	envDbBucketName = "BUCKET"
+	envPortName     = "PORT"
+	dbPathName      = "DB_PATH"
+)
 
-func main() {
-	// Outputs usage if number of arguments are off
-	//
-	if len(os.Args) > 4 || len(os.Args) < 2 {
-		fmt.Println("usage: " + os.Args[0] + " db_path [port] [db_bucket_name]")
-		return
+func getConfig() core.Config {
+	envPort, cP := os.LookupEnv(envPortName)
+	bucket, cB := os.LookupEnv(envDbBucketName)
+	dbFile, cD := os.LookupEnv(dbPathName)
+	if !cP || !cB || !cD {
+		fmt.Errorf("enviroment variables: [%s, %s, %s] are required...", envDbBucketName, envPortName, dbPathName)
+	}
+	port, err := strconv.ParseUint(envPort, 10, 64)
+	if err != nil {
+		fmt.Errorf("PORT environment variable must be an unsigned integer, value was: %s", envPort)
 	}
 
-	// First argument is the filepath of the database
-	//
-	dbFile := os.Args[1]
-
-	// Second (optional) argument is the the port number
-	//
-	port := defaultPort
-	if len(os.Args) > 2 {
-		// Some sanity check for the port argument before using
-		//
-		var err error
-		port, err = strconv.ParseUint(os.Args[2], 10, 64)
-		if err != nil {
-			fmt.Println("port argument must be an unsigned integer")
-			return
-		}
-
-		if port < uint64(49152) || port > uint64(65535) {
-			fmt.Println("port argument must be a value between 49152–65535")
-			return
-		}
-	}
-
-	// Third (optional) argument is the the bucket name
-	//
-	dbBucketName := defaultDbBucketName
-	if len(os.Args) > 3 {
-		dbBucketName = os.Args[3]
-	}
-
-	// Configure and run the application
-	//
-	dbCfg := db.Config{
-		FilePath:   dbFile,
-		BucketName: dbBucketName,
+	if port < uint64(49152) || port > uint64(65535) {
+		fmt.Errorf("PORT environment variable must be a value between 49152–65535, value was: %s", envPort)
 	}
 
 	coreCfg := core.Config{
-		DbCfg:        dbCfg,
+		DbCfg: db.Config{
+			FilePath:   dbFile,
+			BucketName: bucket,
+		},
 		Port:         port,
 		EnableLogger: true,
 		Standalone:   true,
 	}
+	return coreCfg
+}
 
-	core.Run(coreCfg)
+func main() {
+	core.Run(getConfig())
 }
